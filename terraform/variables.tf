032d160ab062eca229785e14df736cbb2977646c
gavin-a-admin <git@gavinalan.com>
2026-06-15 11:50:34 -0400

    feat: Phase 1 - Terraform scaffold with Azure resource group module

---
+++ terraform/variables.tf
@@ -0,0 +1,26 @@
+﻿variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "azure-homelab-rg"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
+
+variable "tags" {
+  description = "Resource tags"
+  type        = map(string)
+  default = {
+    project     = "azure-homelab"
+    environment = "dev"
+    owner       = "gavin-alan"
+  }
+}
